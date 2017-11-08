37f71b6396fbacfa7a5f6cd0c12de22260c642c9
Karl Stolley <karl.stolley@gmail.com>
2017-11-07 20:16:57 -0600

    Expand <abbr> tags on a click event

--- js/site.js
+++ js/site.js
@@ -4,3 +4,10 @@ $('a[href^="http"]').on('click', function(event) {
   window.open($(this).attr('href'), "_blank");
   event.preventDefault();
 });
+
+$('abbr[title]').one('click', function() {
+  var expansion = $(this).attr('title');
+  var abbr = $(this).text();
+  $(this).text(expansion + " (" + abbr + ")");
+  $(this).addClass('is-expanded');
+});
